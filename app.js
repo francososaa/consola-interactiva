a8dd9937961045588bab737152b12edc69a1083b
Franco Sosa <soosaf22@gmail.com>
2023-03-15 14:55:03 -0300

    -

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
+
 require('colors');
+const { inquirerMenu, 
+        pausa,
+        leerInput 
+} = require('./helpers/inquirer');
 
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { guardarDB,
+        leerDB
+} = require('./helpers/guardarArchivo');
 
-console.clear();
-
+const Tareas  = require('./models/tareas');
 
 const main = async() => {
 
     let opt = '';
+    const tareas = new Tareas();
 
+    const tareasDB = leerDB();
+
+    if ( tareasDB ){
+
+    }
+    await pausa();
     do {
-        opt = await mostrarMenu();
-        console.log({ opt });
-        
-        if ( opt !== '0') await pausa();
+        opt = await inquirerMenu();
+
+        switch ( opt ){
+            case '1':
+                const desc = await leerInput('Descripcion: ');
+                tareas.crearTarea( desc );
+            break;
+
+            case '2':
+                console.log( tareas.listadoArr );
+            break;
+        }
+
+        //guardarDB( tareas.listadoArr );
+        await pausa();
         
     } while (opt !=='0');
     
